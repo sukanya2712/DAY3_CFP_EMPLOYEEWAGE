4bbb6b93bc820e8af28f85198df369bc68894518
sukanya2712 <sukanyanaik511@gmail.com>
2023-06-10 15:44:36 +0530

    added uc6

--- Program.cs
+++ Program.cs
@@ -6,14 +6,17 @@
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HRS_IN_MONTH = 10;
         static void Main(string[] args)
         {
             
             int empHrs=0;
-            int empWage=0;
-            int totalEmpWage = 0;
-            for(int day=0;day< NUM_OF_WORKING_DAYS; day++)
+            int totalWorkingDays=0;
+            int totalEmpHrs = 0;
+
+            while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays< NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -31,10 +34,10 @@
                         empHrs = 0;
                         break;
                 }
-                empWage = EMP_RATE_PER_HOUR * empHrs;
-                totalEmpWage += empWage;
-                Console.WriteLine($"Wage of the employee is :- {empWage}");
+                totalEmpHrs += empHrs;
+                Console.WriteLine("DAY:"+totalWorkingDays+" EMPHOURS:"+ empHrs);
             }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total empWage :" + totalEmpWage);
         }
     }
