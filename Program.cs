f8b5f88aa334ecc4896ae82e72edd31bd5807ca6
sukanya2712 <sukanyanaik511@gmail.com>
2023-06-10 14:44:58 +0530

    addeduc3

--- Program.cs
+++ Program.cs
@@ -4,12 +4,18 @@
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs;
             int empWage;
             Random random = new Random();
-            int empCheck = random.Next(0,2);
+            int empCheck = random.Next(0,3);
+            if (empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("Employee is present");
+                empHrs = 4;
+            }
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is present");
